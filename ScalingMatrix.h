d04b62fa1c2789e05f7656e2274523c520195993
ttrng2702 <tgt2702@gmail.com>
2024-12-15 22:53:39 -0800

    Update ScalingMatrix.h

--- ScalingMatrix.h
+++ ScalingMatrix.h
@@ -1,1 +1,12 @@
+#ifndef SCALINGMATRIX_H
+#define SCALINGMATRIX_H
+
+#include "Matrix.h"
+
+class ScalingMatrix : public Matrix {
+public:
+    ScalingMatrix(float scaleFactor);
+};
+
+#endif // SCALINGMATRIX_H
 

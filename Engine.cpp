283865d2708b962a18bc2c6ffe7b0df0e73b1b78
ttrng2702 <tgt2702@gmail.com>
2024-12-15 22:55:46 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,26 +1,51 @@
-SRC_DIR := .
-OBJ_DIR := .
-SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp)
-OBJ_FILES := $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SRC_FILES))
-LDFLAGS := -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
-CXXFLAGS := -g -Wall -fpermissive -std=c++17 -Iinclude
-TARGET := mandelbrot.out
+#include "Engine.h"
 
-# MacOS should compile this using homebrew's sfml
-# brew install sfml
-OS_NAME := $(shell uname -s)
-ifeq ($(OS_NAME), Darwin)
-        LDFLAGS += -L/opt/homebrew/lib
-        CXXFLAGS += -I/opt/homebrew/include
-endif
+Engine::Engine() : m_Window(sf::VideoMode(800, 600), "Particle System") {
+    m_Window.setFramerateLimit(60);
+}
 
-$(TARGET): $(OBJ_FILES)
-	g++ -o $@ $^ $(LDFLAGS)
+void Engine::run() {
+    sf::Clock clock;
 
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	g++ $(CXXFLAGS) -c -o $@ $<
-run:
-	./$(TARGET)
+    while (m_Window.isOpen()) {
+        input();
+        float dt = clock.restart().asSeconds();
+        update(dt);
+        render();
+    }
+}
 
-clean:
-	rm $(TARGET) $(OBJ_DIR)/*.o
+void Engine::input() {
+    sf::Event event;
+    while (m_Window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            m_Window.close();
+        }
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        sf::Vector2i mousePos = sf::Mouse::getPosition(m_Window);
+        Particle p(m_Window, rand() % 26 + 25, mousePos); 
+        m_particles.push_back(p);
+    }
+}
+
+void Engine::update(float dt) {
+    for (auto& p : m_particles) {
+        p.update(dt);
+    }
+    
+    // Remove particles that have expired
+    m_particles.erase(
+        std::remove_if(m_particles.begin(), m_particles.end(), [](const Particle& p) { return !p.isAlive(); }),
+        m_particles.end()
+    );
+}
+
+void Engine::render() {
+    m_Window.clear();
+    for (const auto& p : m_particles) {
+        m_Window.draw(p);
+    }
+    m_Window.display();
+}
